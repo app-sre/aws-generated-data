503d678fe212a3f431b0c2cf16ff1e4745b63c95
nbauer <nbauer@redhat.com>
2023-11-08 11:49:46 -0500

    formatting

--- aws_generated_data/commands/rds_eol.py
+++ aws_generated_data/commands/rds_eol.py
@@ -29,8 +29,8 @@ class RdsItem(BaseModel):
     eol: date
 
     def __init__(self, **data: Any):
-        if 'version' in data:
-            data['version'] = data['version'].rstrip('*')
+        if "version" in data:
+            data["version"] = data["version"].rstrip("*")
         super().__init__(**data)
 
     def __lt__(self, other: Any) -> bool:
