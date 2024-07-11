369ce197b5c9dabd74ed6b3819cdd85882d6d4fb
Christian Assing <cassing@redhat.com>
2024-07-11 11:37:23 +0200

    :bug: rds: fix finding the table with minor versions

--- aws_generated_data/commands/rds_eol.py
+++ aws_generated_data/commands/rds_eol.py
@@ -59,9 +59,22 @@ def engine_with_url(value: str) -> Engine:
 def parse_aws_release_calendar(page: str) -> list[CalItem]:
     items: list[CalItem] = []
     soup = BeautifulSoup(page, "html5lib")
-    # the first table is the one we want
-    version_table = soup.find("table")
-    if not version_table:
+
+    if not (
+        minor_version_section := soup.find(
+            lambda tag: tag.get("id", "")
+            in {
+                # IDs from header minor version headings
+                "aurorapostgresql.minor.versions.supported",
+                "PostgreSQL.Concepts.VersionMgmt.Supported",
+                "MySQL.Concepts.VersionMgmt.Supported",
+            }
+        )
+    ):
+        raise RuntimeError("Failed to find minor version section")
+
+    # the first table in the minor version section is the one we want
+    if not (version_table := minor_version_section.find_next("table")):
         raise RuntimeError("Failed to find version table")
 
     for row in version_table.find_all("tr"):  # type: ignore
